bf9fd6cf56402944789f63f87de303569bdb1746
Robert Safin <robertsafin98@gmail.com>
2023-04-01 22:42:57 +0800

    css modules

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,19 @@
-import React from 'react';
 
-import './Button.css';
+// import './Button.css';
+import styles from './Button.module.css'
+
+import React from 'react'
+
+
+
+
+
+
+
 
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
